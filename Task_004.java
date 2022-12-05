21786a7503c3d5087a124d2bf5ee4b6ec2abe9f8
MaxX <33282787+m1644@users.noreply.github.com>
2022-12-05 17:11:58 +0300

    Add Task_004

---
+++ Task_004.java
@@ -0,0 +1,65 @@
+/**
+ * Неявная типизация
+ * Task_001
+public class Task_004 {
+    public static void main(String[] args) {
+        int a = 18;
+        // 10010
+        // a = a >> 1;
+        System.out.println(a >> 1);
+    }
+}
+*/
+
+/**
+ * Task_002
+public class Task_004 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        System.out.println(a | b);  // 7
+        // 101
+        // 010
+        // 111 
+    }    
+}
+ */
+
+/**
+ * Task_003
+public class Task_004 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        System.out.println(a & b);  // 0
+        // 101
+        // 010
+        // 000 
+    }    
+}
+ */
+
+/**
+ * Task_004
+public class Task_004 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        System.out.println(a ^ b);  // 7
+        // 101
+        // 010
+        // 111 
+    }    
+}
+ */
+
+/**
+ * Task_005
+ */
+public class Task_004 {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = true;
+        System.out.println(a & b);
+    }    
+}
